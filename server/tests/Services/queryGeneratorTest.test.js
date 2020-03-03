9a4ab252daaa910496253866a74637b6a44771a4
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-03 21:32:47 +0530

    Added git ignore and test.

--- server/tests/Services/queryGeneratorTest.test.js
+++ server/tests/Services/queryGeneratorTest.test.js
@@ -1,4 +1,5 @@
 const queryGenerator = require('./../../src/Services/queryGenerator');
 test("Should generate perfect query", () => {
-    expect(queryGenerator.generateSPQuery("sp_insertCustomer", ["Pronoy", 1, "Mukherjee", "mam@a", 2])).toBe("CALL sp_insertCustomer('Pronoy',1,'Mukherjee','mam@a',2");
+    expect(queryGenerator.generateSPQuery("sp_insertCustomer", ["Pronoy", 1, "Mukherjee", "mam@a", 2]))
+        .toBe("CALL sp_insertCustomer('Pronoy',1,'Mukherjee','mam@a',2");
 });
