d51ed91b0f51c91f6d8af5d56b2b0495fa8ccea9
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-09 18:33:41 +0530

    Added 2 tables. API Logger and status master.

--- server/src/Loggers/index.js
+++ server/src/Loggers/index.js
@@ -3,13 +3,13 @@ const path = require('path');
 const constants = require('./../Helpers/constants');
 const generator = require('./../Services/generator');
 const logger = {};
-logger.baseDir = path.join(__dirname, "/.Logs");
 /**
  * Method to append the log data to the append files.
  * @param logData: The data to be appended.
  * @returns {Promise<any>}
  */
 logger.appendLogs = (logData) => {
+    logger.baseDir = path.join(__dirname, "/.Logs");
     const formattedDate = generator.generateCurrentTime();
     return new Promise((resolve, reject) => {
         fs.appendFile(logger.baseDir + "/" + constants.LOG_FILE_NAME,
