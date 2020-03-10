86948ea435dd7e485a6eebe4c029c9d07fc4d5c4
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-10 10:47:16 +0530

    Updated the parameters in Api logger SP.

--- DB/StoredProcedures/sp_LogApiStatus.sql
+++ DB/StoredProcedures/sp_LogApiStatus.sql
@@ -1,5 +1,5 @@
 DROP PROCEDURE IF EXISTS sp_LogApiStatus;
-CREATE PROCEDURE sp_LogApiStatus(in requestKey varchar(50), in path varchar(50), in apiStatus INT)
+CREATE PROCEDURE sp_LogApiStatus(in requestKey varchar(50), in path varchar(50), in apiStatus INT,in responseCode INT)
 BEGIN
     #TODO:
 end;
