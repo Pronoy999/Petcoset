8c522a1083801531cd2722dfa2114841dd2da21f
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-10 11:00:35 +0530

    Added Api Key in API logger SP.

--- DB/StoredProcedures/sp_LogApiStatus.sql
+++ DB/StoredProcedures/sp_LogApiStatus.sql
@@ -1,5 +1,5 @@
 DROP PROCEDURE IF EXISTS sp_LogApiStatus;
-CREATE PROCEDURE sp_LogApiStatus(in requestKey varchar(50), in path varchar(50), in apiStatus INT,in responseCode INT)
+CREATE PROCEDURE sp_LogApiStatus(in requestKey varchar(50), in path varchar(50), in apiStatus INT,in responseCode INT,in apiKey varchar(200))
 BEGIN
     #TODO:
 end;
