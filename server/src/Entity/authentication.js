cfaa56ae9083a8443951d842264f7faa5420008a
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-21 17:07:46 +0530

    Added Authentication module.

---
+++ server/src/Entity/authentication.js
@@ -0,0 +1,43 @@
+const database = require('./../Services/databaseService');
+const constants = require('./../Helpers/constants');
+const validators = require('./../Helpers/validators');
+const printer = require('./../Helpers/printer');
+
+class Authentication {
+   /**
+    * _emailId
+    * _password
+    * _role
+    * @param emailId
+    * @param password
+    * @param role
+    */
+   constructor(emailId, password, role) {
+      this._emailId = validators.validateEmail(emailId) ? emailId : false;
+      this._password = validators.validateString(password) ? password : false;
+      this._role = validators.validateString(role) ? role : false;
+   }
+
+   /**
+    * Method to check the validity of the login.
+    * @returns {Promise<Array>}: the details of the user after successful login.
+    */
+   checkValidity() {
+      return new Promise((resolve, reject) => {
+         database.runSp(constants.SP_LOGIN, [this._emailId, this._password])
+            .then(_resultSet => {
+               const result = _resultSet[0][0];
+               resolve(result);
+            }).catch(err => {
+            printer.printError(err);
+            reject(err);
+         });
+      });
+   }
+}
+
+/**
+ * Exporting the authentication class.
+ * @type {Authentication}
+ */
+module.exports = Authentication;
