cd966a37cc695d4deddff72f0244f9d18f9d8712
Abbas Ali <abbasatrix@outlook.com>
2020-03-27 23:54:03 +0530

    sp for SubscriptionServiceBooking has been created

---
+++ DB/StoredProcedures/sp_SubscriptionServiceBooking.sql
@@ -0,0 +1,46 @@
+drop procedure if exists sp_SubscriptionServiceBooking;
+create procedure sp_SubscriptionServiceBooking(par_bookingType enum ('service_booking','subscription_booking'),
+                                               par_customerId int,
+                                               par_subscriptionId varchar(50),
+                                               par_serviceId int,
+                                               par_employeeId int,
+                                               par_vendorId int)
+begin
+    SELECT AUTO_INCREMENT
+            INTO @subscriptionId
+            FROM information_schema.TABLES
+            WHERE TABLE_SCHEMA = (SELECT DATABASE())
+              AND TABLE_NAME = 'tbl_SubscriptionServiceBooking';
+
+    if (par_bookingType = 'subscription_booking')
+    then
+
+        insert into tbl_SubscriptionServiceBooking
+        (booking_type_id,
+         customer_id,
+         subscription_id,
+         service_id,
+         employee_id)
+        values (par_bookingType,
+                par_customerId,
+                par_subscriptionId,
+                par_serviceId,
+                par_employeeId);
+
+        SELECT @subscriptionId as id;
+    else
+        insert into tbl_SubscriptionServiceBooking
+        (booking_type_id,
+         customer_id,
+         subscription_id,
+         service_id,
+         vendor_id)
+        values (par_bookingType,
+                par_customerId,
+                par_subscriptionId,
+                par_serviceId,
+                par_vendorId);
+
+        SELECT @subscriptionId as id;
+    end if;
+end;
