1384a3c56d9e408287415455ac4572cacdf10936
AbbasAtrix94 <abbasatrix@gmail.com>
2020-03-27 18:10:57 +0530

    sp and table for SubscriptionServiceBooking has been created

---
+++ DB/Tables/sp_tbl_SubscriptionServiceBooking.sql
@@ -0,0 +1,35 @@
+DROP PROCEDURE IF EXISTS sp_tbl_SubscriptionServiceBooking;
+DELIMITER $$
+CREATE PROCEDURE sp_tbl_SubscriptionServiceBooking()
+BEGIN
+    DECLARE currentSchema varchar(100);
+    SELECT database() into currentSchema;
+    IF NOT EXISTS(
+            SELECT 1
+            FROM information_schema.TABLES
+            WHERE TABLE_SCHEMA = currentSchema
+              AND TABLE_NAME = 'tbl_SubscriptionServiceBooking'
+        ) THEN
+        CREATE TABLE `tbl_SubscriptionServiceBooking`
+        (
+            `id`              int(11)   NOT NULL AUTO_INCREMENT,
+            `booking_type_id` int(11)        DEFAULT NULL,
+            `customer_id`     int(11)        DEFAULT NULL,
+            `subscription_id` int(11)        DEFAULT NULL,
+            `service_id`      int(11)        DEFAULT NULL,
+            `employee_id`     int(11)        DEFAULT NULL,
+            `vendor_id`       int(11)        DEFAULT NULL,
+            `total_amount`    decimal(18, 2) DEFAULT NULL,
+            `is_active`       int(11)        DEFAULT '1',
+            `created_by`      int(11)        DEFAULT '1',
+            `modified_by`     int(11)        DEFAULT NULL,
+            `created`         timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+            `modified`        timestamp NULL DEFAULT NULL,
+            PRIMARY KEY (`id`)
+        ) ;
+    end if;
+end$$
+
+DELIMITER ;
+CALL sp_tbl_SubscriptionServiceBooking();
+DROP PROCEDURE IF EXISTS sp_tbl_SubscriptionServiceBooking;
