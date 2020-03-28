48ebcaa1800bafd11a3bf4f23e1d1c3d521c4cf7
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-03-28 20:16:45 +0530

    Updated the booking table and SP.

--- DB/StoredProcedures/sp_SubscriptionServiceBooking.sql
+++ DB/StoredProcedures/sp_SubscriptionServiceBooking.sql
@@ -7,39 +7,43 @@ create procedure sp_SubscriptionServiceBooking(par_bookingType enum ('service_bo
                                                par_vendorId int)
 begin
     SELECT AUTO_INCREMENT
-            INTO @subscriptionId
-            FROM information_schema.TABLES
-            WHERE TABLE_SCHEMA = (SELECT DATABASE())
-              AND TABLE_NAME = 'tbl_SubscriptionServiceBooking';
+    INTO @subscriptionId
+    FROM information_schema.TABLES
+    WHERE TABLE_SCHEMA = (SELECT DATABASE())
+      AND TABLE_NAME = 'tbl_SubscriptionServiceBooking';
 
     if (par_bookingType = 'subscription_booking')
     then
 
         insert into tbl_SubscriptionServiceBooking
-        (booking_type_id,
+        (booking_type,
          customer_id,
          subscription_id,
          service_id,
-         employee_id)
+         employee_id,
+         created_by)
         values (par_bookingType,
                 par_customerId,
                 par_subscriptionId,
                 par_serviceId,
-                par_employeeId);
+                par_employeeId,
+                par_customerId);
 
         SELECT @subscriptionId as id;
     else
         insert into tbl_SubscriptionServiceBooking
-        (booking_type_id,
+        (booking_type,
          customer_id,
          subscription_id,
          service_id,
-         vendor_id)
+         vendor_id,
+         created_by)
         values (par_bookingType,
                 par_customerId,
                 par_subscriptionId,
                 par_serviceId,
-                par_vendorId);
+                par_vendorId,
+                par_customerId);
 
         SELECT @subscriptionId as id;
     end if;
--- DB/Tables/sp_tbl_SubscriptionServiceBooking.sql
+++ DB/Tables/sp_tbl_SubscriptionServiceBooking.sql
@@ -13,18 +13,18 @@ BEGIN
         CREATE TABLE `tbl_SubscriptionServiceBooking`
         (
             `id`              int(11)   NOT NULL AUTO_INCREMENT,
-            `booking_type_id` int(11)        DEFAULT NULL,
-            `customer_id`     int(11)        DEFAULT NULL,
-            `subscription_id` int(11)        DEFAULT NULL,
-            `service_id`      int(11)        DEFAULT NULL,
-            `employee_id`     int(11)        DEFAULT NULL,
-            `vendor_id`       int(11)        DEFAULT NULL,
-            `total_amount`    decimal(18, 2) DEFAULT NULL,
-            `is_active`       int(11)        DEFAULT '1',
-            `created_by`      int(11)        DEFAULT '1',
-            `modified_by`     int(11)        DEFAULT NULL,
-            `created`         timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-            `modified`        timestamp NULL DEFAULT NULL,
+            `booking_type`    ENUM ('service_booking','subscription_booking') DEFAULT NULL,
+            `customer_id`     int(11)                                         DEFAULT NULL,
+            `subscription_id` int(11)                                         DEFAULT NULL,
+            `service_id`      int(11)                                         DEFAULT NULL,
+            `employee_id`     int(11)                                         DEFAULT NULL,
+            `vendor_id`       int(11)                                         DEFAULT NULL,
+            `total_amount`    decimal(18, 2)                                  DEFAULT NULL,
+            `is_active`       int(11)   NOT NULL                              DEFAULT '1',
+            `created_by`      int(11)   NOT NULL,
+            `modified_by`     int(11)                                         DEFAULT NULL,
+            `created`         timestamp NOT NULL                              DEFAULT CURRENT_TIMESTAMP,
+            `modified`        timestamp,
             PRIMARY KEY (`id`)
         ) ;
     end if;
