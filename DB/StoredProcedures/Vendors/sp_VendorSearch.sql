cc04dfb063c0d5b7de54dd6b6e8696014dd2e008
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-04-25 14:15:48 +0530

    Added Vendor Search by Status filter.

--- DB/StoredProcedures/Vendors/sp_VendorSearch.sql
+++ DB/StoredProcedures/Vendors/sp_VendorSearch.sql
@@ -1,17 +1,26 @@
 drop procedure if exists sp_VendorSearch;
-create procedure sp_VendorSearch(IN par_email varchar(100), IN par_phno varchar(100), IN par_id int)
+create procedure sp_VendorSearch(IN par_email varchar(100), IN par_phno varchar(100), IN par_id int,
+                                 in parStatus varchar(30))
 BEGIN
-    if (length(par_email) < 5 and length(par_phno) < 10 and par_id < 1)
+    if (length(par_email) < 5 and length(par_phno) < 10 and par_id < 1 and length(parStatus) < 0)
     then
         SELECT -1 as id;
     else
+        set @statusId = 0;
+        if length(parStatus) > 0 THEN
+            select id into @statusId from tbl_StatusMaster where status_name LIKE parStatus;
+        end if;
+        #select @statusId;
         SELECT concat(
-                       'SELECT C.id, first_name, last_name, email, phone_number, gender, address_1, address_2, CM.city_name,sm.state_name, pincode
+                       'SELECT C.id, first_name, last_name, email, phone_number, gender, address_1, address_2,
+                        CM.city_name,sm.state_name, pincode,C.status_id,s.status_name
                FROM tbl_VendorMaster C
                 left join tbl_CityMaster CM
                     on CM.id=C.city
                 left join tbl_StateMaster sm
                     on sm.id=CM.state_id
+                left join tbl_StatusMaster s
+                    on C.status_id = s.id
                WHERE C.is_active=1 ',
                        CASE
                            WHEN length(par_email) > 5 THEN concat(' and C.email = ''', par_email, '''')
@@ -21,13 +30,19 @@ BEGIN
                            WHEN length(par_phno) > 10 THEN concat(' and C.phone_number =''', par_phno, '''')
                            ELSE ''
                            END
-                   , CASE WHEN par_id > 0 THEN concat(' and C.id = ', par_id) ELSE '' END
+                   , CASE
+                         WHEN par_id > 0 THEN concat(' and C.id = ', par_id)
+                         ELSE '' END
+                   , case
+                         when @statusId > 0 THEN concat(' and C.status_id = ', @statusId)
+                         ELSE ''
+                           END
                    )
         into @dySQL;
         #select @dySQL;
         PREPARE stmt_Growth FROM @dySQL;
-		EXECUTE stmt_Growth;
-		DEALLOCATE PREPARE stmt_Growth;
+        EXECUTE stmt_Growth;
+        DEALLOCATE PREPARE stmt_Growth;
     end if;
 
 END;
--- DB/Tables/sp_tbl_VendorMaster.sql
+++ DB/Tables/sp_tbl_VendorMaster.sql
@@ -18,9 +18,9 @@ BEGIN
             `email`        varchar(255)   NOT NULL,
             `phone_number` varchar(13)    NOT NULL,
             `gender`       enum ('M','F') NOT NULL,
-            `address_1`    varchar(255)   DEFAULT NULL,
-            `address_2`    varchar(255)   DEFAULT NULL,
-            `city`         int(11)        DEFAULT NULL,
+            `address_1`    varchar(255)        DEFAULT NULL,
+            `address_2`    varchar(255)        DEFAULT NULL,
+            `city`         int(11)             DEFAULT NULL,
             `pincode`      int(11)             DEFAULT NULL,
             `status_id`    int(11)             DEFAULT NULL,
             `is_active`    tinyint(4)          DEFAULT '1',
@@ -33,26 +33,29 @@ BEGIN
             KEY `idx_email_vendor` (`email`)
         );
     END If;
-        IF NOT EXISTS(
-                select 1
-                FROM information_schema.COLUMNS
-                WHERE TABLE_SCHEMA = currentSchema
-                  and TABLE_NAME = 'tbl_VendorMaster'
-                  and COLUMN_NAME = 'profile_image'
-            ) THEN
-            BEGIN
-                ALTER TABLE tbl_VendorMaster
-                    ADD COLUMN profile_image varchar(255) DEFAULT NULL after pincode;
-            end;
-        end if;
-        IF NOT EXISTS(
-            select 1 from information_schema.COLUMNS WHERE TABLE_SCHEMA=currentSchema
-            and TABLE_NAME='tbl_VendorMaster'
-            and COLUMN_NAME='rating'
-            ) THEN
-            BEGIN
-                ALTER TABLE tbl_VendorMaster add column rating double(5,1) default 0.0 after profile_image;
-            end;
+    IF NOT EXISTS(
+            select 1
+            FROM information_schema.COLUMNS
+            WHERE TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_VendorMaster'
+              and COLUMN_NAME = 'profile_image'
+        ) THEN
+        BEGIN
+            ALTER TABLE tbl_VendorMaster
+                ADD COLUMN profile_image varchar(255) DEFAULT NULL after pincode;
+        end;
+    end if;
+    IF NOT EXISTS(
+            select 1
+            from information_schema.COLUMNS
+            WHERE TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_VendorMaster'
+              and COLUMN_NAME = 'rating'
+        ) THEN
+        BEGIN
+            ALTER TABLE tbl_VendorMaster
+                add column rating double(5, 1) default 0.0 after profile_image;
+        end;
     end if;
 end $$
 
