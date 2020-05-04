4807e3e655aa34ac7d4b917d8e5694836bd032e4
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-05-04 22:40:22 +0530

    Added Cluster module for Multi CPU.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-#!/usr/bin/.env node
+#!/usr/bin/env node
 const server = require('./src/Helpers/server');
+const os = require('os');
+const cluster = require('cluster');
 require('dotenv').config();
 const app = {};
 app.init = () => {
-    server.init();
+   const cpuLength = os.cpus().length;
+   for (let i = 0; i < cpuLength; i++) {
+      if (cluster.isMaster) {
+         cluster.fork();
+      } else {
+         server.init();
+      }
+   }
 };
 //Starting the App.
 app.init();
